b9a3db2b0e8eb65c92b2ccd0beaa2c0da5573728
AibarSh <157199468+AibarSh@users.noreply.github.com>
2024-12-21 18:08:12 +0500

    Update News.java

--- Platform/News.java
+++ Platform/News.java
@@ -1,28 +1,30 @@
 package Platform;
-
+import java.util.*;
 public class News {
-	private int newsID;
-	private String news;
-
+	private Vector<Integer> newsID = new Vector<>();
+	private Vector<String> news = new Vector<>();
+	protected static int idCounter = 0;
 	public News(int n, String s) {
-		this.newsID = n;
-		this.news = s;
+		this.newsID.add(n);
+		this.news.add(s);
 	}
 
-	public int getNewsID() {
+	public Vector<Integer> getNewsID() {
 		return newsID;
 	}
 
-	public void setNewsID(int newsID) {
-		this.newsID = newsID;
+	public void setNewsID(int n, int n1) {
+		this.newsID.set(n, n1);
 	}
 
-	public String getNews() {
-		return news;
+	public void getNews() {
+		for(int i=0; i<newsID.size(); i++) {
+			System.out.println(news.get(i));
+		}
 	}
 
-	public void setNews(String news) {
-		this.news = news;
+	public void setNews(int n,String s) {
+		this.news.set(n,s);
 	}
 
 	@Override
@@ -33,4 +35,3 @@ public class News {
 				'}';
 	}
 }
-
