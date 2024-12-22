8c785df0cfdc834c02a5345acce30e28fef72ea0
AibarSh <157199468+AibarSh@users.noreply.github.com>
2024-12-22 14:13:05 +0500

    Update UserDecorator.java

--- ResearchWork/UserDecorator.java
+++ ResearchWork/UserDecorator.java
@@ -1,6 +1,9 @@
 package ResearchWork;
 
+import Platform.Appeals;
+import Platform.News;
 import Users.User;
+import Users.UserDatabase;
 
 public class UserDecorator extends User{
 
@@ -14,6 +17,14 @@ public class UserDecorator extends User{
 
 	public User getDecoratedUser() {
 		return this.decoratedUser;	
+	}
+	
+	public String showCommands() {
+		return super.showCommands();
+	}
+	
+	public void console(News news, Journal journal, Appeals appeals, UserDatabase database) {
+		super.console(news, journal, appeals, database);
 	}
 	
 }
