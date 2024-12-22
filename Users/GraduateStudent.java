6217e1f2aaf9777a7d3ce226ee9a776d1eb68acf
AibarSh <157199468+AibarSh@users.noreply.github.com>
2024-12-22 13:22:21 +0500

    Update GraduateStudent.java

--- Users/GraduateStudent.java
+++ Users/GraduateStudent.java
@@ -66,6 +66,150 @@ public class GraduateStudent extends Student {
 
 	@Override
 	public String showCommands() {
-		return super.showCommands() + "13 - Get Degree | 14 -  Get Diploma Projects | ";
+		return super.showCommands() + "10 - Get Degree Details | 11 -  Get Diploma Projects List | ";
+	}
+
+	public void console(News news, Journal journal, Appeals appeals) {
+		InputUtil inputint = new InputUtil();
+		InputUtil inputstr = new InputUtil();
+		boolean running = true;
+		while(running) {
+
+			System.out.println(showCommands());
+			int input = inputint.getIntInput("Enter your command: \n");
+
+			if(input == 0) {
+				running = false;
+				System.out.println("=====                     Exiting...                   =====");
+				break;
+			}
+
+			switch(input) {
+				case(1):
+					try {
+						int inpt = inputint.getIntInput("| English - 1 | Russian - 2 | Kazakh - 3 | ");
+						if (inpt < 4 && inpt > 0) {
+							if (inpt == 1) {
+								setLanguage(Languages.English);
+
+							}
+							else if (inpt == 2) {
+								setLanguage(Languages.Russian);
+
+							}
+							else {
+								setLanguage(Languages.Kazakh);
+
+							}
+
+						}
+						else System.out.println("| Invalid input, try again | ");
+
+					}
+					catch(Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(2):
+					try {
+						String bait = inputstr.getStringInput(""); // нужен для очистки стрима
+						String contents = inputstr.getStringInput("Enter request contents: ");
+						Request req = new Request(contents);
+						appeals.addToRequestList(req);
+						System.out.println("| Request send | \n");
+					}
+
+					catch(Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(3):
+					try {
+						news.getNews();
+					}
+
+					catch(Exception e ) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+
+				case(4):
+					try {
+						journal.viewJournal();
+
+					}
+					catch(Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(5):
+					try {
+						String bait = inputstr.getStringInput(""); // нужен для очистки стрима
+						String currentPassword = inputstr.getStringInput("Enter your current password: ");
+						String newPassword = inputstr.getStringInput("\nEnter your new password: ");
+						setPassword(currentPassword, newPassword);
+
+					}
+					catch(Exception e) {
+
+					}
+					break;
+
+				case(6):
+					try {
+						viewCourses();
+					} catch (Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(7):
+					try{
+						System.out.println("List of Available Courses: \n");
+						viewCourseList();
+						String courseIdReg = inputstr.getStringInput("Enter the course you want to register: ");
+						registrationForCourses(courseIdReg);
+					} catch (Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(8):
+					try {
+						String subject = inputstr.getStringInput("Enter the subject which journal you want to see: ");
+						displayJournal(subject);
+					} catch (Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(9):
+					try{
+						viewMarks();
+					} catch (Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(10):
+					try{
+						getDegreeDetails();
+					} catch (Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+					break;
+
+				case(11):
+					try{
+						getDiplomaProjectsList();
+					} catch (Exception e) {
+						System.out.println("| Error occured... | \n");
+					}
+			}
+		}
 	}
 }
